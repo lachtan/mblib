f60d267e32331dfe6c9268edee15dff30fa5807e
Martin Blazik <blazik@premiumsms.cz>
2010-12-23 16:21:42 +0100

    Zakladni koncept pro praci se socketem

--- mbl/net/socket.py
+++ mbl/net/socket.py
@@ -4,44 +4,31 @@ TODO
 - vlastnosti socketu
 - timeout
 - pouziti select
+
+jak sem pak nacpat socket pair minimalne kvuli testovani
 """
 
 from __future__ import absolute_import
 import socket
 import select
-from mbl.io.stream import InputStream, OutputStream
-from mbl.io.timeout import TimeoutError
+from mbl.io import InputStream, OutputStream, IOStream
+from mbl.io import TimeoutError
 
 
 # ------------------------------------------------------------------------------
-# SocketClient
+# Socket
 # ------------------------------------------------------------------------------
 
-class SocketClient(object):
-	def __init__(self, address):
+class Socket(object):
+	def __init__(self, socket, address):
 		self.__address = address
-		self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.setConnectTimeout(None)
-		self.setTimeout(None)
-		self.__inputStream = SocketInputStream(self.__socket, self.__timeout)
-		self.__outputStream = SocketOutputStream(self.__socket, self.__timeout)
+		self.__socket = socket
+		self.__inputStream = SocketInputStream(self.__socket)
+		self.__outputStream = SocketOutputStream(self.__socket)
 
 
-	def connect(self):
-		self.__socket.settimeout(self.__connectTimeout)
-		try:
-			self.__socket.connect(self.__address)
-		except socket.timeout:
-			raise TimeoutError('connect', self.__connectTimeout)
-		self.__socket.setblocking(0)
-
-
-	def setConnectTimeout(self, timeout):
-		self.__connectTimeout = timeout
-
-
-	def setTimeout(self, timeout):
-		self.__timeout = timeout
+	def address(self):
+		return self.__address
 
 
 	def inputStream(self):
@@ -52,12 +39,45 @@ class SocketClient(object):
 		return self.__outputStream
 
 
-	def close(self):
-		self.__socket.close()
+	def ioStream(self):
+		return IOStream(self.__inputStream, self.__outputStream)
 
 
 	def shutdown(self):
 		self.__socket.shutdown(socket.SHUT_RDWR)
+		self.__outputStream.close()
+		self.__socket.close()
+
+
+	def close(self):
+		self.__inputStream.close()
+		self.__outputStream.close()
+		self.__socket.close()
+
+
+# ------------------------------------------------------------------------------
+# SocketClient
+# ------------------------------------------------------------------------------
+
+class SocketClient(object):
+	def __init__(self, address):
+		self.__address = address
+		self.setConnectTimeout(None)
+
+
+	def connect(self):
+		_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		_socket.settimeout(self.__connectTimeout)
+		try:
+			_socket.connect(self.__address)
+		except socket.timeout:
+			raise TimeoutError('connect', self.__connectTimeout)
+		#_socket.setblocking(0)
+		return Socket(_socket, self.__address)
+
+
+	def setConnectTimeout(self, timeout):
+		self.__connectTimeout = timeout
 
 
 # ------------------------------------------------------------------------------
@@ -99,9 +119,61 @@ class SocketInputStream(InputStream):
 
 class SocketOutputStream(OutputStream):
 	def __init__(self, socket, timeout):
+		super(SocketOutputStream, self).__init__()
 		self.__socket = socket
+		self.__timeout = timeout
 
 
-	def write(self, text):
-		return self.__socket.sendall(text)
+	def write(self, data):
+		super(SocketOutputStream, self).write(data)
+		startTime = time()
+		writtenBytes = 0
+		while True:
+			now = time()
+			duration = now - startTime
+			if duration > self.__timeout:
+				raise TimeoutError
+			timeout = self.__timeout - duration
+			self.__waitForWrite(timeout)
+			bytes = self.__socket.send(data[writtenBytes:])
+			writtenBytes += bytes
+			if writtenBytes == len(data):
+				return
 
+
+	def __waitForWrite(self, timeout):
+		status = select([], [self.__socket], [], timeout)
+		if len(status[1]) == 0:
+			raise TimeoutError
+
+
+	def _write(self, data):
+		super(SocketOutputStream, self).write(data)
+		self.__socket.sendall(data)
+
+
+# ------------------------------------------------------------------------------
+#
+# ------------------------------------------------------------------------------
+
+class SocketServer(object):
+	def __init__(self, serverAddress):
+		self.__serverAddress = serverAddress
+		self.__serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		# a nejake dalsi srandicky
+		# s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+		# kde tohle volat?
+		self.__serverSocket.bind()
+		self.__serverSocket.listen(1)
+		# timeouty!
+
+
+	def accept(self)
+		clientSocket, clientAddress = self.__socket.accept()
+		return Socket(clientSocket, clientAddress)
+
+
+	def mainLoop(self, callback, args):
+		while True:
+			clientSocket = self.accept()
+			callback(clientSocket, args)
